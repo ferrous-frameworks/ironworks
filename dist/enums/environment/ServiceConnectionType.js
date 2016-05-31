84c09cb71d9476037797f73d5f6669da06f55c0f
Tyson Hester <tyson_hester@homedepot.com>
2016-05-31 10:53:40 -0400

    updated dist

---
+++ dist/enums/environment/ServiceConnectionType.js
@@ -0,0 +1,9 @@
+var ServiceConnectionType = (function () {
+    function ServiceConnectionType() {
+    }
+    ServiceConnectionType.core = "core";
+    ServiceConnectionType.component = "component";
+    return ServiceConnectionType;
+})();
+module.exports = ServiceConnectionType;
+//# sourceMappingURL=ServiceConnectionType.js.map
